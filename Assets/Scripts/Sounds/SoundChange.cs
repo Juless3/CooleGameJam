23df30137661fa914974bb09bd2df631e56bab3c
Xeha97 <jason.schlueter97@gmail.com>
2022-12-13 13:42:59 +0100

    FMOD implemented
    Soundtracks
    Footsteps
    
    UI Pading bei den Buttons

---
+++ Assets/Scripts/Sounds/SoundChange.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using FMODUnity;
+using UnityEngine;
+
+public class SoundChange : MonoBehaviour
+{
+    [SerializeField] private StudioEventEmitter TrackOutOnEnter;
+    [SerializeField] private StudioEventEmitter TrackOnOnEnter; 
+ 
+    
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+       TrackOutOnEnter.SetParameter("Fade", 0);
+       TrackOnOnEnter.SetParameter("Fade", 1);
+    }
+    
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        TrackOutOnEnter.SetParameter("Fade", 1);
+        TrackOnOnEnter.SetParameter("Fade", 0);
+    }
+}
